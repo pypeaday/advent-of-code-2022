624ce590b11c9176e515b4ad7dffd6caba34b1d0
Pypeaday <pypeaday@pm.me>
2022-12-07 11:56:41 -0600

    wip day7 part 1 - works for sample data, but not puzzle data

--- advent_of_code_2022/day7.py
+++ advent_of_code_2022/day7.py
@@ -3,7 +3,14 @@
 """
 
 from pathlib import Path
-from typing import Dict, List, Set
+from typing import Dict, List
+
+
+def get_commands() -> List[List[str]]:
+    # raw = Path("./data/day7.sample").read_text()
+    raw = Path("./data/day7.data").read_text()
+    commands = [x for x in raw.split("$") if x]
+    return [c.strip() for c in commands]
 
 
 class Directory:
@@ -11,10 +18,34 @@ class Directory:
         self.name = name
         self.files: Dict[str, File] = {}
         self.parent: Directory = None
-        self.children: List[Directory] = []
+        self.children: Dict[str, Directory] = dict()
 
-    def total_size(self):
+    @property
+    def contents(self):
+        return [*[f for f in self.files.values()], *[d for d in self.children.values()]]
+
+    def total_file_size(self):
         return sum([file.size for file in self.files.values()])
+
+    def total_child_directory_size(self):
+        return sum([d.total_file_size() for d in self.children.values()])
+
+    def total_contents_size(self):
+        return self.total_file_size() + self.total_child_directory_size()
+
+
+#     def total_size_of_children(self, total: int = 0):
+#         if not self.children:
+#             return total
+#         else:
+#             for child in self.children:
+#                 total += child.total_size_of_children(total)
+#             return total
+
+#     def total_size_with_children(self):
+#         total = self.total_size()
+#         children_total = self.total_size_of_children()
+#         return total + children_total
 
 
 class File:
@@ -30,16 +61,6 @@ class Device:
         self.old_pwd = None
         self.cmd = "cd"
 
-    def determine_line(self, s: str):
-        if s[0] == "$":
-            return "command"
-        elif s[:2] == "ls":
-            return "ls"
-        elif s[0] == "d":
-            return "dir"
-        elif s[0].isdigit():
-            return "file"
-
     def cddotdot(self, s: str):
         if self.pwd.name == "/":
             print("at / already")
@@ -54,28 +75,48 @@ class Device:
         else:
             directory_name = s.split(" ")[-1]
             self.old_pwd = self.pwd
-            self.pwd = directories.get(directory_name, Directory(directory_name))
-        print(f"problem with {s}")
+            self.pwd = self.directories.get(directory_name, Directory(directory_name))
+            self.pwd.parent = (
+                self.old_pwd if self.pwd.parent is None else self.pwd.parent
+            )
+            self.old_pwd.children.update({self.pwd.name: self.pwd})
+            return 0
+        breakpoint()
         return 1
 
-    def ls(self, s: str):
-        line = self.determine_line(s)
-        if line == "dir":
-            self.directories.add(Directory(s.split(' ')[-1]))
-        elif line == "file":
-            filename = s.split(" ")[1]
-            size = int(s.split(" "))[0]
-            self.pwd.files.update({filename: File(filename, size)})
+    def ls(self, elements: List[str]):
+        for element in elements:
+            dtype = "directory" if element.split(" ")[0] == "dir" else "file"
+            name = element.split(" ")[1]
+            if dtype == "directory":
+                if name not in [k for k in self.directories.keys()]:
+                    self.directories[name] = Directory(name)
+            elif dtype == "file":
+                size = int(element.split(" ")[0])
+                self.pwd.files.update({name: File(name, size)})
 
 
-directories = [
-    Directory(x)
-    for x in Path("./data/day7.sample").read_text().split("\n")
-    if x[:3] == "dir"
-]
+def setup_device():
 
-for directory in directories:
-    # for each directory, parse input again and find all lines between `cd directory` and cd anything else
-    for line in Path("./data/day7.sample").read_text().split("\n"):
-        if line == f"$ cd {directory.name}":
+    device = Device()
 
+    for cmd in get_commands():
+        if "cd" in cmd.split("\n")[0]:
+            target = cmd.split("\n")[-1]
+            device.cd(target)
+        if "ls" in cmd.split("\n")[0]:
+            output = cmd.split("\n")[1:]
+            device.ls(output)
+
+    return device
+
+
+def main():
+    device = setup_device()
+
+    total = 0
+    for directory in device.directories.values():
+        if directory.total_contents_size() <= 100_000:
+            total += directory.total_contents_size()
+
+    print(f"{total=}")
